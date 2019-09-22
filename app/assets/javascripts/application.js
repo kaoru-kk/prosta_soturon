93983dcc2e58e6c8f1fbd3a95fa88c6e7bbaaece
19980410 <kkaoru1998@gmail.com>
2019-09-22 11:42:25 +0000

    グラフ

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,15 +12,11 @@
 //
 //= require Chart.min
 //= require rails-ujs
-//= require activestorage
 //= require turbolinks
+//= require activestorage
 //= require_tree .
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
 
-$(window).bind("load", function(){
-    if(document.URL.indexOf("users/show.html.erb")) {
-        $('a').attr('id','show_a');
-    }
-});
+
